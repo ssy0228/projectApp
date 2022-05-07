c2b0dc42764fef850e186a93da51eab29791dc36
ssy0228 <alchemist0228@gmail.com>
2022-05-07 16:41:40 +0900

    resize body and elements

--- js/signup.js
+++ js/signup.js
@@ -1,10 +1,16 @@
 function nextpage() {
   const main = document.querySelector("main");
+  const prev = document.querySelector('#prev');
+  const next = document.querySelector('#next');
   const btn = document.querySelector("section.page>button.next");
   const reset = document.querySelector("section.confirm>button[type='reset']");
   const submit = document.querySelector("section.confirm>button[type='submit']");
+
+
   btn.addEventListener("click", () => {
-    main.style.transform = "translateX(-100vw)";
+    main.style.transform = "translateX(-375px)";
+    prev.style.opacity = 0;
+    next.style.opacity = 1;
   });
   reset.addEventListener("click", () => {
     main.style.transform = "translateX(0)";
